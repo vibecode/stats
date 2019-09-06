61c18495e8dbc89b5f66121a2ae7b6cf167aa13b
vibecode <rumashkin@gmail.com>
2019-09-06 14:49:12 +0200

    add html report

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import { MatchReader } from './MatchReader'
 import { ConsoleReport } from './reportTargets/consoleReport'
 import { WinsAnalysis } from './WinsAnalayzer'
 import { Summary } from './Summary'
+import { HtmlReport } from './HtmlReport'
 
 const reader = new CsvFilereader('football.csv')
 const matchReader = new MatchReader(reader)
 
 matchReader.load()
 
-const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport())
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport())
 
 summary.buildAndPrintReport(matchReader.data)
